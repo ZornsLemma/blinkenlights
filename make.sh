045879d5294cb07c5397904688fa34d40c932865
Steven Flintham <sgf@lemma.co.uk>
2020-12-07 17:03:29 +0000

    Tidy repository a bit

--- make.sh
+++ make.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-beebasm -w -v -i run.asm -do blinkenlights.ssd -opt 3 > run.lst
-beebasm -w -v -i run7.asm -do blinkenlights7.ssd -opt 3 > run7.lst
+for x in panels/*.png; do
+	python encode-panel.py "$x" "panels/$(basename $x .png).bin"
+done
+beebasm -w -v -i top.asm -do blinkenlights.ssd -opt 3 > top.lst
