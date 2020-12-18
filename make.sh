5418252bba49eef7b06b9b0781bb606003b8afb7
Steven Flintham <sgf@lemma.co.uk>
2020-12-18 04:13:03 +0000

    Add TODo

--- make.sh
+++ make.sh
@@ -12,3 +12,4 @@ python encode-led-shapes.py $TMP/led-shapes.asm
 python make-led-freq-spread.py $TMP/led-freq-spread.asm
 python make-random-tables.py $TMP/random-tables.asm
 beebasm -w -v -i top.asm -do ../blinkenlights.ssd -opt 3 > ../top.lst
+# TODO: Add a LICENCE file? Seems a bit overkill but probably a good idea. MIT?
